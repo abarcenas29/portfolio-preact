a7a61628745960b2d8ea31d8c90251271e70ae5a
Aldrich Allen Barcenas <aldrich.barcenas@gmail.com>
2017-06-26 00:31:31 +0800

    portfolio page skeleton complete

--- src/containers/Portfolio/index.js
+++ src/containers/Portfolio/index.js
@@ -9,7 +9,12 @@ import css from 'styled-components'
 
 import Waypoint from 'react-waypoint'
 
-import { Grid, Header, Image } from 'semantic-ui-react'
+import {
+  Button,
+  Card,
+  Grid,
+  Header
+} from 'semantic-ui-react'
 import { NoPaddingGrid } from 'components/CommonJS'
 
 import { dimNavMenuAction } from 'containers/App/actions'
@@ -65,9 +70,140 @@ class Portfolio extends Component {
         </Waypoint>
         <PortfolioContainer>
           <br />
-          <Grid columns={1}>
-            <PortfolioItem />
-          </Grid>
+          <NoPaddingGrid columns={1} centered>
+            <Grid.Column widescreen={8} computer={16}>
+              <NoPaddingGrid columns={1} centered>
+                <Grid.Column
+                  computer={13}
+                  widescreen={16}
+                  mobile={16}>
+                  <Header as='h2'>Work Projects</Header>
+                  <p>
+                    There are some of the projects I've worked in during my career. I didn't include all of them since they're mainly derivatives of these projects. These are my milestone projects where I used new solutions and techniques in developing apps.
+                  </p>
+                </Grid.Column>
+                <PortfolioItem
+                  title='Be Cash Ready'
+                  color='blue'
+                  subheader='SPA using React and NodeJS'
+                  platform='web'
+                  background='/assets/images/becashready.png'>
+                  <p>
+                    Pork venison leberkas t-bone jowl. Ham hock filet mignon sausage ball tip jerky turducken alcatra pork chop tongue tenderloin chuck rump turkey. Pastrami pancetta short loin flank porchetta, venison sausage ham. Tenderloin cow picanha fatback, hamburger rump swine landjaeger.
+                  </p>
+                  <div>
+                    <Button inverted basic>
+                      Description
+                    </Button>
+                  </div>
+                </PortfolioItem>
+                <PortfolioItem
+                  title='A-nest Consulting'
+                  color='green'
+                  subheader='Web Application using Angular 1 and FuelPHP'
+                  platform='web'
+                  background='assets/images/anest-consulting.jpg'>
+                  <p>
+                    Pork venison leberkas t-bone jowl. Ham hock filet mignon sausage ball tip jerky turducken alcatra pork chop tongue tenderloin chuck rump turkey. Pastrami pancetta short loin flank porchetta, venison sausage ham. Tenderloin cow picanha fatback, hamburger rump swine landjaeger.
+                  </p>
+                  <div>
+                    <Button inverted basic>
+                      Description
+                    </Button>
+                  </div>
+                </PortfolioItem>
+                <PortfolioItem
+                  title='Sumi Lab'
+                  color='brown'
+                  subheader='Web Application using JQuery and FuelPHP'
+                  background='/assets/images/sumilab.jpg'
+                  platform='web'>
+                  <p>
+                    Pork venison leberkas t-bone jowl. Ham hock filet mignon sausage ball tip jerky turducken alcatra pork chop tongue tenderloin chuck rump turkey. Pastrami pancetta short loin flank porchetta, venison sausage ham. Tenderloin cow picanha fatback, hamburger rump swine landjaeger.
+                  </p>
+                  <div>
+                    <Button inverted basic>
+                      Description
+                    </Button>
+                  </div>
+                </PortfolioItem>
+              </NoPaddingGrid>
+            </Grid.Column>
+            <Grid.Column widescreen={8} computer={16}>
+              <NoPaddingGrid columns={1} centered>
+                <Grid.Column
+                  computer={13}
+                  widescreen={8}
+                  mobile={16}>
+                  <Header as='h2'>Hobby Projects</Header>
+                  <p>
+                    Projects that I do in my spare time. Normally improve on the techniques I learned at work, or create new ones from articles and blog posts I've read.
+                  </p>
+                </Grid.Column>
+                <PortfolioItem
+                  title='MeganeMe'
+                  color='orange'
+                  subheader='Web Application using JQuery and FuelPHP'
+                  background='/assets/images/megane-me.jpg'
+                  platform='web'>
+                  <p>
+                    Pork venison leberkas t-bone jowl. Ham hock filet mignon sausage ball tip jerky turducken alcatra pork chop tongue tenderloin chuck rump turkey. Pastrami pancetta short loin flank porchetta, venison sausage ham. Tenderloin cow picanha fatback, hamburger rump swine landjaeger.
+                  </p>
+                  <div>
+                    <Button inverted basic>
+                      Description
+                    </Button>
+                    <Button inverted basic>
+                      Github
+                    </Button>
+                  </div>
+                </PortfolioItem>
+              </NoPaddingGrid>
+            </Grid.Column>
+            <Grid.Column widescreen={8} computer={16}>
+              <NoPaddingGrid columns={1} centered>
+                <Grid.Column
+                  computer={13}
+                  widescreen={8}
+                  mobile={16}>
+                  <Header as='h2'>Github Projects</Header>
+                  <p>
+                    Some of my Github projectst that are mainly porducts, but more of toolset.
+                  </p>
+                </Grid.Column>
+                <Grid.Column computer={4}>
+                  <Card>
+                    <Card.Content>
+                      <Card.Header>AAA</Card.Header>
+                      <Card.Description>
+                        Bacon ipsum dolor amet tenderloin cupim cow, venison pork belly doner rump ham hock shank pork loin kevin alcatra biltong burgdoggen meatball. Cupim kielbasa sirloin turducken flank tongue beef kevin ball tip ribeye bacon pork.
+                      </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                      <Button size='small'>
+                        Github
+                      </Button>
+                    </Card.Content>
+                  </Card>
+                </Grid.Column>
+                <Grid.Column computer={4}>
+                  <Card>
+                    <Card.Content>
+                      <Card.Header>AAA</Card.Header>
+                      <Card.Description>
+                        Bacon ipsum dolor amet tenderloin cupim cow, venison pork belly doner rump ham hock shank pork loin kevin alcatra biltong burgdoggen meatball. Cupim kielbasa sirloin turducken flank tongue beef kevin ball tip ribeye bacon pork.
+                      </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                      <Button size='small'>
+                        Github
+                      </Button>
+                    </Card.Content>
+                  </Card>
+                </Grid.Column>
+              </NoPaddingGrid>
+            </Grid.Column>
+          </NoPaddingGrid>
         </PortfolioContainer>
       </PortfolioPageContainer>
     )
--- src/containers/Portfolio/sections/PortfolioItem.js
+++ src/containers/Portfolio/sections/PortfolioItem.js
@@ -1,19 +1,47 @@
 import React from 'react'
-// import css from 'styled-components'
-import { Grid, Header, Image } from 'semantic-ui-react'
+import css from 'styled-components'
+import { Grid, Header } from 'semantic-ui-react'
+import { NoPaddingGrid } from 'components/CommonJS'
 
-const PortfolioItem = () => {
+const ImageColumn = css(Grid.Column)`
+  background-image: url('${props => props.bg}');
+  background-size: cover;
+
+  @media only screen and (max-width: 767px) {
+    min-height: 10em;
+  }
+`
+
+const PortfolioItem = ({
+  background,
+  children,
+  color,
+  title,
+  subheader
+}) => {
   return (
-    <Grid.Column
-      color='red'
-      computer={13}
-      widescreen={8}
-      mobile={16}>
-      <Header as='h2'>AAA</Header>
-      <Image fluid bordered>
-        <img src='/assets/images/anest-consulting.jpg' />
-      </Image>
-      <p>AAA</p>
+    <Grid.Column computer={13} widescreen={16} mobile={16}>
+      <NoPaddingGrid
+        relaxed
+        columns={2}
+        reversed='mobile'
+        stackable
+        padded='mobile'>
+        <Grid.Column computer={5} mobile={16} color={color}>
+          <Header as='h2' inverted dividing>
+            {title}
+            <Header.Subheader>
+              {subheader}
+            </Header.Subheader>
+          </Header>
+          {children}
+        </Grid.Column>
+        <ImageColumn
+          bg={background}
+          computer={11}
+          mobile={16}
+        />
+      </NoPaddingGrid>
     </Grid.Column>
   )
 }
